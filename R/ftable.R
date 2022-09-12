4c4680cd908b1448b99b2b79c7c9f3ba758e8362
yingjiexue <test@runoob.com1476713911@qq.com>
2022-09-13 00:59:08 +0800

    update

--- R/ftable.R
+++ R/ftable.R
@@ -34,10 +34,18 @@ fmmodel<-function(tab,ftsz=9,ftname="Time New Roma"){
     newtb<-tab
   }
 
-  names(newtb)<-as.character(1:ncol(newtb))
   sposit<-which(newtb=="(1)",arr.ind = T)[1,1]-1
   obs<-which(newtb=="Observations",arr.ind = T)[1,1]
-  ft<-flextable(newtb)
+  bodyc<-newtb[(sposit+2):nrow(newtb),]
+  names(bodyc)<-as.character(c(" ",newtb[sposit+1,2:ncol(newtb)]))
+
+  ft<-flextable(bodyc)
+  for(adhd in sposit:1){
+    ft<-add_header_row(ft,values = as.vector(t(newtb[adhd,])),
+                       colwidths = rep(1,ncol(newtb)))
+  }
+
+
   for(rowns in 1:sposit){
     namva<-unique(t(newtb[rowns,2:ncol(newtb)])%>%as.vector())
     for(cols in 1:length(namva)){
@@ -67,20 +75,20 @@ fmmodel<-function(tab,ftsz=9,ftname="Time New Roma"){
         for(idex in unique(recod$grup)){
           mtop<-max(recod$ids[recod$grup==idex])
           mtao<-min(recod$ids[recod$grup==idex])
-          ft<-ft%>%merge_at(i=rowns,j=mtao:mtop)
+          ft<-ft%>%merge_at(i=rowns,j=mtao:mtop,part = "header")
         }
       }
     }
   }
   ft<-border_remove(ft)
-  ft<-delete_part(ft,part="header")
   ft <- set_table_properties(ft, layout = "autofit")
   def_par <- fp_par(text.align = "center")
-  ft<-style(ft,1:nrow(newtb),2:ncol(newtb),pr_p=def_par)
+  ft<-style(ft,pr_p=def_par,part = "header")
+  ft<-style(ft,1:nrow(bodyc),2:ncol(bodyc),pr_p=def_par,part = "body")
   def_cell<-fp_border(color = "black",width = 1)
-  ft<-surround(ft,c(1,obs),1:ncol(newtb),border.top=def_cell)
-  ft<-surround(ft,sposit+1,1:ncol(newtb),border.bottom =def_cell)
-  ft<-surround(ft,nrow(newtb),1:ncol(newtb),border.bottom =def_cell)
+  ft<-surround(ft,1,1:ncol(newtb),border.top=def_cell,part = "header")
+  ft<-surround(ft,c(1,obs-sposit-1),1:ncol(newtb),border.top=def_cell)
+  ft<-surround(ft,nrow(bodyc),1:ncol(bodyc),border.bottom =def_cell)
   ft<-add_footer_lines(ft,stri_encode("\u6ce8\uff1a\u62ec\u53f7\u5185\u4e3a\u56de\u5f52\u7cfb\u6570\u5bf9\u5e94\u7684t\u7edf\u8ba1\u91cf\u3002\u5176\u4e2d***\u3001**\u548c*\u5206\u522b\u4ee3\u8868\u7cfb\u6570\u57281%\u30015%\u548c10%\u7684\u7f6e\u4fe1\u6c34\u5e73\u4e0b\u663e\u8457\u3002"))
   ft<-font(ft,fontname=stri_escape_unicode(ftname),part="all")
   ft<-font(ft,fontname="\u5b8b\u4f53",part="footer")
