3b4441d135369946be7b120234a9018d155255ea
yingjiexue <test@runoob.com1476713911@qq.com>
2022-12-13 19:54:08 +0800

    update202212

--- R/ftable.R
+++ R/ftable.R
@@ -139,13 +139,15 @@ fmmodel<-function(tab,langu="C",tabname="",notehead="",notefoot="",ftsz=9,ftname
       ft<-surround(ft,c(obs-sposit-2,nrow(bodyc)),1:ncol(bodyc),border.bottom =def_cell,part = "body")
       ft<-add_footer_lines(ft,paste("Note:",notefoot,"t-statistics are given in parentheses. \u002a\u002a\u002a, \u002a\u002a and \u002a represent statistical significance at the 1%, 5% and 10% level, respectively."))
     }else{
-      heads<-paste0(tabname,"\n",notehead)
+      heads<-ifelse(tabname=="",notehead,
+                    ifelse(notehead=="",tabname,
+                           paste0(tabname,"\n",notehead)))
       ft<-add_header_lines(ft,values =heads )
       ft<-border_remove(ft)
       def_parf <- fp_par(text.align = "justify")
       ft<-style(ft,1,2:ncol(bodyc),pr_p=def_parf,part = "header")
       def_par <- fp_par(text.align = "center")
-      ft<-style(ft,2:sph,2:ncol(bodyc),pr_p=def_par,part = "header")
+      ft<-style(ft,2:(sph+1),2:ncol(bodyc),pr_p=def_par,part = "header")
       ft<-style(ft,1:nrow(bodyc),2:ncol(bodyc),pr_p=def_par,part = "body")
       def_cell<-fp_border(color = "black",width = 1)
       ft<-surround(ft,2,1:ncol(newtb),border.top=def_cell,part = "header")
